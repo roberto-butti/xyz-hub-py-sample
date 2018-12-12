f4da1f54e402d2fbf356cdd17a2392ef08f32166
Roberto Butti <roberto.butti@gmail.com>
2018-12-12 21:50:07 +0100

    Spaces and Features

--- xyz.py
+++ xyz.py
@@ -1,52 +1,56 @@
 import json
-import requests
-import argparse
 
+import argparse
+from Rb.Models.Project import Project
+from Rb.Models.Space import Space
+from Rb.Models.Feature import Feature
+
+
+
+p = Project()
 
 __author__ = "Roberto B."
 
-base_url = "https://xyz.api.here.com/"
-base_url_projects = base_url + "project-api/projects/"
-base_url_spaces = base_url + "hub/spaces"
-rot = ""
 
 parser = argparse.ArgumentParser(description='XYZ Studio Project Inspector.')
 parser.add_argument('--pid', help='Project Identifier')
+parser.add_argument('--sid', help='Space Identifier')
+parser.add_argument('--rot', help='ROT, Read Only Token')
 args = parser.parse_args()
 print(args.pid)
+print(args.sid)
+print(args.rot)
 project_id = args.pid
 
-
-
-def get_json(url, token = None):
-    headers = {}
-    if ( token != None ):
-        headers = {'Authorization': 'Bearer ' + token}
-
-    with requests.get(url, headers = headers) as response:
-        data = json.loads(response.text)
-        print(json.dumps(data, indent=4))
-    return data
-
-
-
-
 try:
-    print("Retrieving project " + project_id)
-    data = get_json(base_url_projects+project_id)
+    if args.pid:
+        project = Project()
+        print("Retrieving project " + project_id)
+        p = project.get_project(project_id)
+        rot = project.rot
 
-    rot = data["rot"]
-    print(rot)
-    print("Retrieving spaces")
-    data = get_json(base_url_spaces, rot)
+        project_id = p["id"]
+        print("Project: " + p["id"] + " Read access only token: " + rot)
+        print("Retrieving spaces")
+        space = Space()
+        space.rot = rot
+        spaces = space.get_spaces()
+        for s in spaces:
+            print("Space:" + s["id"] + " - " + s["title"])
 
-    for d in data:
-        my_url = base_url_spaces+"/"+d["id"]+"/statistics"
-        print(my_url)
-        stats = get_json(my_url, rot)
 
 
-    print("ok")
+    if args.sid and args.rot:
+        space = Space()
+        s = space.get_space(args.sid, args.rot)
+        print(s)
+        feature = Feature()
+        f = feature.get_features(args.sid, args.rot)
+        print(f)
+
+
+
+    print("Well done!")
 
 
 

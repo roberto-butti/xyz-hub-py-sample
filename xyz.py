4906a08356061b2b7b99f8418717c35ba0d34518
Roberto Butti <roberto.butti@gmail.com>
2018-12-13 20:57:16 +0100

    Args commands for Spaces and Projects

--- xyz.py
+++ xyz.py
@@ -11,49 +11,49 @@ p = Project()
 
 __author__ = "Roberto B."
 
+def input_project(args):
+    print(args.pid)
+    project_id = args.pid
+    project = Project()
+    print("Retrieving project " + project_id)
+    p = project.get_project(project_id)
+    rot = project.rot
 
-parser = argparse.ArgumentParser(description='XYZ Studio Project Inspector.')
-parser.add_argument('--pid', help='Project Identifier')
-parser.add_argument('--sid', help='Space Identifier')
-parser.add_argument('--rot', help='ROT, Read Only Token')
+    project_id = p["id"]
+    print("Project: " + p["id"] + " Read access only token: " + rot)
+    print("Retrieving spaces")
+    space = Space()
+    space.rot = rot
+    spaces = space.get_spaces()
+    for s in spaces:
+        print("Space:" + s["id"] + " - " + s["title"])
+
+
+
+def input_space(args):
+    space = Space()
+    s = space.get_space(args.sid, args.rot)
+    print(s)
+    feature = Feature()
+    f = feature.get_features(args.sid, args.rot)
+    print(f)
+
+
+parser = argparse.ArgumentParser(prog='xyz', description='XYZ Studio Project Inspector.')
+subparsers = parser.add_subparsers(help='sub-command help')
+
+parser_project = subparsers.add_parser('project', help='Managing XYZ Projects')
+parser_project.add_argument('--pid', help='Project Identifier')
+parser_project.set_defaults(func=input_project)
+
+parser_space = subparsers.add_parser('space', help='Managing XYZ Spaces')
+parser_space.add_argument('--sid', help='Space Identifier')
+parser_space.add_argument('--rot', help='ROT, Read Only Token')
+parser_space.set_defaults(func=input_space)
+
 args = parser.parse_args()
-print(args.pid)
-print(args.sid)
-print(args.rot)
-project_id = args.pid
-
-try:
-    if args.pid:
-        project = Project()
-        print("Retrieving project " + project_id)
-        p = project.get_project(project_id)
-        rot = project.rot
-
-        project_id = p["id"]
-        print("Project: " + p["id"] + " Read access only token: " + rot)
-        print("Retrieving spaces")
-        space = Space()
-        space.rot = rot
-        spaces = space.get_spaces()
-        for s in spaces:
-            print("Space:" + s["id"] + " - " + s["title"])
+args.func(args)
 
 
-
-    if args.sid and args.rot:
-        space = Space()
-        s = space.get_space(args.sid, args.rot)
-        print(s)
-        feature = Feature()
-        f = feature.get_features(args.sid, args.rot)
-        print(f)
-
-
-
-    print("Well done!")
-
-
-
-except:
-    print("There was an error")
+print("Well done!")
 

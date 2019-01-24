03224d9f37d026f7b3b015256a1d1c54dc6d6098
Roberto B <roberto.butti@gmail.com>
2019-01-24 21:54:17 +0100

    Feature iterate

--- Rb/Models/Feature.py
+++ Rb/Models/Feature.py
@@ -1,4 +1,5 @@
 from Rb.Models.BaseModel import BaseModel
+import json
 
 
 class Feature(BaseModel):
@@ -6,11 +7,13 @@ class Feature(BaseModel):
 
     def __init__(self, config):
         super().__init__(config)
-        self.base_url_feature = self.base_url + "hub/spaces/{0}/search"
+        self.base_url_feature = self.base_url + "hub/spaces/{0}/iterate"
 
+    # https://xyz.api.here.com/hub/static/swagger/#/Read_Features/iterateFeatures
     def get_features(self, sid, rot=None):
         url = self.base_url_feature.format(sid)
         if rot is None:
             rot = self.rot
         f = self.api.get_json(url, rot, 'application/geo+json')
-        return f
+
+        return json.dumps(f, indent=2)

f45c7b9c0165c1ce37d324cae627b7606193c0f5
Roberto B <roberto.butti@gmail.com>
2019-02-16 08:33:22 +0100

    fix PEP8 xyz.py file

--- xyz.py
+++ xyz.py
@@ -31,13 +31,13 @@ def input_project(args):
             layers = p["layers"]
             for l in layers:
                 print("## Layer " + l["meta"]["title"])
-                print("### Space : " + l["geospace"]["id"] )
+                print("### Space : " + l["geospace"]["id"])
 
             #print("Retrieving spaces")
             #space = Space(config)
             #space.rot = rot
             #spaces = space.get_spaces()
-            #for s in spaces:
+            # for s in spaces:
             #    print("Space:" + s["id"] + " - " + s["title"])
     else:
         project = Project(config)
@@ -66,12 +66,11 @@ def input_space(args):
             print(s)
 
 
-
-
 def input_features(args):
     feature = Feature(config)
     f = feature.get_features(args.sid, args.rot)
     print(f)
+
 
 def input_fit(args):
     fit = Fit(config)
@@ -82,26 +81,32 @@ def input_fit(args):
         print("NOTHING")
 
 
-
-parser = argparse.ArgumentParser(prog='xyz', description='XYZ Studio Project Inspector.')
-subparsers = parser.add_subparsers(help='Managing projects, spaces, feautres', dest='operation')
+parser = argparse.ArgumentParser(
+    prog='xyz', description='XYZ Studio Project Inspector.')
+subparsers = parser.add_subparsers(
+    help='Managing projects, spaces, feautres', dest='operation')
 
 parser_project = subparsers.add_parser('project', help='Managing XYZ Projects')
 parser_project.add_argument('--pid', help='Project Identifier')
-#parser_project.add_argument('show')
-parser_project.add_argument('--show', action='store_const', const=True, help='Show also the Project Json')
-parser_project.add_argument('--spaces', action='store_const', const=True, help='Show also the list of spaces related to the Project')
+# parser_project.add_argument('show')
+parser_project.add_argument(
+    '--show', action='store_const', const=True, help='Show also the Project Json')
+parser_project.add_argument('--spaces', action='store_const', const=True,
+                            help='Show also the list of spaces related to the Project')
 
 parser_project.set_defaults(func=input_project)
 
 parser_space = subparsers.add_parser('space', help='Managing XYZ Spaces')
-parser_space.add_argument('--list', action='store_const', const=True, help='Show all spaces')
+parser_space.add_argument('--list', action='store_const',
+                          const=True, help='Show all spaces')
 parser_space.add_argument('--sid', help='Space Identifier')
 parser_space.add_argument('--rot', help='ROT, Read Only Token')
-parser_space.add_argument('--statistics', action='store_const', const=True, help='Include Statistics')
+parser_space.add_argument(
+    '--statistics', action='store_const', const=True, help='Include Statistics')
 parser_space.set_defaults(func=input_space)
 
-parser_features = subparsers.add_parser('feature', help='Managing XYZ Features')
+parser_features = subparsers.add_parser(
+    'feature', help='Managing XYZ Features')
 parser_features.add_argument('--sid', help='Space Identifier')
 parser_features.add_argument('--fid', help='Feature Identifier')
 parser_features.add_argument('--rot', help='ROT, Read Only Token')
@@ -120,4 +125,3 @@ else:
     args.func(args)
 
 # print("Well done!")
-

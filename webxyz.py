c1a33eafb6a1d4b169c3ce67c17d8d559b10a2c7
Roberto B <roberto.butti@gmail.com>
2019-02-16 19:10:23 +0100

    Api for Projects (added also Cors)

--- webxyz.py
+++ webxyz.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 from Rb.web.controllers import controllers
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 
 @app.route('/')
 def main():
   c = controllers()
-  return c.project_list()
+  return render_template('index.html')
+
+@app.route("/api/v1/projects")
+def api_list_projects():
+  c = controllers()
+  return jsonify(c.project_list())
+
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+  return jsonify('pong!')
 
